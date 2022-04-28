afe5720b7548ca0e9642718e0df3e6b8b94444e6
Angela Mitchell <anglarenmit@gmail.com>
2022-04-27 21:56:56 -0500

    create dynamic request handler for helloWithQueryParam method

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,22 +2,32 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    // Handles requests at /hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+    // Handles requests at path /hello
+    // Note: you cannot have multiple controller methods pointing to the same path
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
-    // Handles requests at /goodbye
+    // Handles requests at path /goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // Handles request of the form /hello?name=LaunchCode
+    // Example of creating a dynamic request handler
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
